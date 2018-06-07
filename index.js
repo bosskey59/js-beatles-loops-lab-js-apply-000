9ac179c25e9bb704c35a9eb1f53f3e7b409cc024
Alejandro Aguilar <alejandro.aguilar.jr@gmail.com>
2018-06-07 18:43:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function theBeatlesPlay( musicians, instruments){
-  let array =[]
+  let array =[];
   for (let x=0; x<musicians.length; x++){
-    array.push(musicians[x]+' plays '+instruments[x])
+    array.push(musicians[x]+' plays '+instruments[x]);
   }
-  return array
+  return array;
 }
 
 function johnLennonFacts(facts){
-  let array=[]
-  let x = 0 
+  let array=[];
+  let x = 0; 
   while ( x<facts.length ){
-   array.push( facts[x] + "!!!") 
+   array.push( facts[x] + "!!!"); 
    x++;
   }
-  return array
+  return array;
 }
