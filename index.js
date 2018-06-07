bf98d6a5ef066fbec1d7d57e9d225259c4871276
Alejandro Aguilar <alejandro.aguilar.jr@gmail.com>
2018-06-07 18:25:47 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function theBeatlesPlay( musicians, instruments){
+  let array =[]
+  for (let x=0; x<musicians.length; x++){
+    array.push(musicians[x]+' plays '+instruments[x])
+  }
+  return array
+}
+
+function johnLennonFacts(facts){
+  let array=[]
+  let x = 0 
+  while ( x<facts.length ){
+   array.push( facts[x] + "!!!") 
+   x++;
+  }
+  return array
+}
